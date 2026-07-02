97d82ffe383d998a6e6f85c7f34ab0a76f25ffaf
desonglll <lindesong666@163.com>
2026-07-02 11:02:18 +0800

    init hammerspoon.

---
+++ README.md
@@ -0,0 +1,17 @@
+
+## HammerSpoon official website
+
+https://www.hammerspoon.org/
+
+## Using this configuration
+
+```shell
+# https://github.com/Hammerspoon/hammerspoon/issues/579
+defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/.hammerspoon/init.lua"
+
+# OR
+
+mkdir -p ~/.hammerspoon
+
+ln ~/.config/.hammerspoon/init.lua ~/.hammerspoon/init.lua
+```
---
+++ init.lua
@@ -0,0 +1,45 @@
+function reloadConfig(files)
+  doReload = false
+  for _,file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      doReload = true
+    end
+  end
+  if doReload then
+    hs.reload()
+  end
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+    hs.alert.show("Hello World!")
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "E", function()
+    hs.application.open("org.gnu.Emacs")
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "W", function()
+    hs.application.open("com.tencent.xinWeChat")
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "C", function()
+    hs.application.open("com.google.Chrome")
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "M", function()
+    hs.application.open("com.netease.163music")
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "F", function()
+    hs.application.open("com.electron.lark")
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "T", function()
+    hs.application.open("org.alacritty")
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "O", function()
+    hs.application.open("md.obsidian")
+end)
